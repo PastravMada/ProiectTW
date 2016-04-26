c6182e0bfa69bcd087b8952829ba62996c7a752b
PastravMada <pastrav.madalina@gmail.com>
2016-04-26 23:30:06 +0300

    Login and Register Front-end

--- tw/index.php
+++ tw/index.php
@@ -2,46 +2,58 @@
 <html>
 <head>
 	<link rel="shortcut icon" type="image/x-icon" href="logo.ico" />
-	<link rel="stylesheet" type="text/css" href="Incluziuni/incluziuni.css">
-	<link href="CSS/imgSlide.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" type="text/css" href="Incluziuni/incluziuni.css">
+	<link rel="stylesheet" type="text/css" href="css/register.css">
 	<script src="script.js" type="text/javascript"></script>
 </head>
 <body>
-<header>
-	<img src="flower.jpg" width="1350" height="400">	
-</header>
-	<a href="cautareAvansata.php"><div class="wrap">
-		<img src="http://yzyperfumes.com/images/background2.jpg" id="categorie1" alt="Parfum" width="100" height="500"/>
-		<p class="desc" id="parf1">Cautare avansata</h3>
-	</div></a>
-	<section>
-	<article id="logform" align="center">
-			<div class="log">
-				<h3>Login</h3>
-				<form action="back-end/login-backend.php?error=$er" method="post">					
-					<fieldset>
-						<br>
-							<div ><label  for="username">Username: </label></div>
-							<div ><input type="text" id="username" name="username" placeholder="Username" value=""/></div>
-						<br>
-							<div ><label  for="password">Password: </label></div>
-							<div ><input type="password" id="password" name="password" placeholder="Password" value=""/></div>
-						<br>
-							
-						<div>
-							<label for="remember" id="remember">
-								<input type="checkbox" name="remember" id="remember" value="1"/>Remember me
-							</label>
-						</div>
-					</fieldset>
-						<p id="logButton">
+	<img src="flower.png" id="imagineLogin">
+	<img src="bee.png" id="imagineLogin2">
+	<p id="textLogin">Wellcome to FII Garden!</p>
+	<div id="wrapper">
+
+	<div id="register" class="animate form">
+	<form align="center" action="back-end/login-backend.php?error=$er" method="post" autocomplete="on">
+
+			<h1>Login</h1>
+				<?php if(isset($_GET['error'])){
+							$er=$_GET['error'];
+							if($er=='logForSell'){
+								echo "<p style='font-size:10px;color:red;'>Trebuie sa fiti logat pentru a efectua cumpararea!</p>";
+							}
+						}
+						if(isset($_GET['message'])){
+							$message=$_GET['message'];
+							if($message=='register'){
+								echo "<p style='font-size:10px;color:green;'>Contul d-voastra a fost creat. Va rugam sa va logati.</p>";
+							}
+						}
+						?>
+								
+					
+							<p>
+							<label  for="username">Username: </label>
+							<input type="text" id="username" name="username" placeholder="Username" value=""/>
+							</p>
+						
+							<p>
+							<label  for="password">Password: </label>
+							<input type="password" id="password" name="password" placeholder="Password" value=""/>
+							</p>
+
+			
+						<p id="logButton" class="signin button">
 							<input id="submit" type="submit" value="Log In"/> 
 						</p>
-					</form>
+				
 					
-			</div>
-			<p id="registerP">Nu aveti cont? Creati-va unul  <a style="color:blue;"href="register.php">aici!</a></p>
-		</article>
-	</section>
+
+			<p id="registerP" class="change_link"> Nu aveti cont? Creati-va unul
+			  <a class="to_register" href="register.php">aici!</a>
+			</p>
+		</form>
+	</div>
+</div>
+
 </body>
 </html>
