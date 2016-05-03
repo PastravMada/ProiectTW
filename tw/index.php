8d7957d13ef64d5381e281e9e6c9cf15fb6d6faa
mVeronica <holoca.veronica@gmail.com>
2016-05-03 09:09:05 -0700

    Update index.php

--- tw/index.php
+++ tw/index.php
@@ -16,35 +16,20 @@
 	<form align="center" action="back-end/login-backend.php?error=$er" method="post" autocomplete="on">
 
 			<h1>Login</h1>
-				<?php if(isset($_GET['error'])){
-							$er=$_GET['error'];
-							if($er=='logForSell'){
-								echo "<p style='font-size:10px;color:red;'>Trebuie sa fiti logat pentru a efectua cumpararea!</p>";
-							}
-						}
-						if(isset($_GET['message'])){
-							$message=$_GET['message'];
-							if($message=='register'){
-								echo "<p style='font-size:10px;color:green;'>Contul d-voastra a fost creat. Va rugam sa va logati.</p>";
-							}
-						}
-						?>
-								
-					
-							<p>
-							<label  for="username">Username: </label>
-							<input type="text" id="username" name="username" placeholder="Username" value=""/>
-							</p>
+					<p>
+					<label  for="username">Username: </label>
+					<input type="text" id="username" name="username" placeholder="Username" value=""/>
+					</p>
 						
-							<p>
-							<label  for="password">Password: </label>
-							<input type="password" id="password" name="password" placeholder="Password" value=""/>
-							</p>
+					<p>
+					<label  for="password">Password: </label>
+					<input type="password" id="password" name="password" placeholder="Password" value=""/>
+					</p>
 
 			
-						<p id="logButton" class="signin button">
-							<input id="submit" type="submit" value="Log In"/> 
-						</p>
+					<p id="logButton" class="signin button">
+						<input id="submit" type="submit" value="Log In"/> 
+					</p>
 				
 					
 
