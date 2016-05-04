f24ad88e8a9f8ead5576084572d2b49c78ef5604
mVeronica <holoca.veronica@gmail.com>
2016-05-04 08:31:19 -0700

    Update creare_comentarii.sql

--- tw/ORACLE/bd/creare_comentarii.sql
+++ tw/ORACLE/bd/creare_comentarii.sql
@@ -4,7 +4,8 @@ DROP TABLE COMENTARII;
 CREATE TABLE COMENTARII(
   id_user NUMBER,
   id_planta NUMBER,
-  comentariu VARCHAR2(200),
-  FOREIGN KEY(id_user) REFERENCES USERI(id_user),
-  FOREIGN KEY(id_planta) REFERENCES PLANTE(id_planta)
+  comentariu VARCHAR2(200)
 );
+
+INSERT INTO COMENTARII(id_user,id_planta,comentariu)VALUES(8,3,'cea mai frumoasa floare');
+INSERT INTO COMENTARII(id_user,id_planta,comentariu)VALUES(9,4,'superba floare');
