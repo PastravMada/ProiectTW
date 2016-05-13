d872e5fb9fd8f5b4a3d1a4af37eb63b370e52de7
madalinavieru11 <mada_lina383@yahoo.com>
2016-05-13 17:37:51 +0300

    Update updated.php

--- tw/updated.php
+++ tw/updated.php
@@ -1,23 +1,10 @@
 <?php
     $conn=oci_connect("mihaela","veronica","localhost/XE");
 	$user=$_POST['id_user'];
-	$stid = oci_parse($conn,$sql);
-    $r = oci_execute($stid);
-	  if($r) 
-                {
-					oci_commit($conn);
-                    echo 'trigger ok...<br>';
-                }
-                else
-                {
-					$m = oci_error($stid);
-                    echo "Error Save [".$m['message']."]";
-                    echo 'trigger not ok...<br>';
-                }
     if(isset($_POST['id_user'])&&isset($_POST['username'])&&
     isset($_POST['password'])&&isset($_POST['nume'])&&
     isset($_POST['prenume'])&&isset($_POST['email'])&&
-    isset($_POST['sex'])&&isset($_POST['oras'])&&isset($_POST['adresa']))
+    isset($_POST['sex'])&&isset($_POST['adresa'])&&isset($_POST['telefon']))
     {
         $id_user = $_POST['id_user'];
         $username = $_POST['username'];
@@ -26,11 +13,11 @@
         $prenume = $_POST['prenume'];
         $email = $_POST['email'];
         $sex = $_POST['sex'];
-        $oras = $_POST['oras'];
-		$adresa = $_POST['adresa'];
+        $adresa = $_POST['adresa'];
+		$telefon = $_POST['telefon'];
 		
         $sql = "UPDATE useri SET id_user='$id_user',username='$username',password='$password',nume='$nume',prenume='$prenume',email='$email',
-								sex='$sex',oras='$oras',adresa='$adresa' WHERE id_user='$id_user'";
+								sex='$sex',adresa='$adresa',telefon='$telefon' WHERE id_user='$id_user'";
                 $stid = oci_parse($conn,$sql);
                 $r = oci_execute($stid);
                 if($r) 
