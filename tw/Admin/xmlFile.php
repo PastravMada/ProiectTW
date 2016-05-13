800cc87e94461496716b0a27e829091967d87116
madalinavieru11 <mada_lina383@yahoo.com>
2016-05-13 16:05:37 +0300

    Create xmlFile.php

---
+++ tw/Admin/xmlFile.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" type="image/x-icon" href="logo.ico" />
+	<link rel="stylesheet" type="text/css" href="IncluziuniAdmin/incluziuniAdmin.css">
+</head>
+<body>
+	<?php include('IncluziuniAdmin/headerAdmin.php');?>
+	<section>
+		<div><a href="csv-back-end.php?com=1">Incarca XML</a></div>
+		<div><a href="csv-back-end.php?com=2">Exporta XML</a></div>
+	</section>
+</body>
+</html>
