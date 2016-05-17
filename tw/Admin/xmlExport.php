719c8949043006c0b76fadd5206d04e82b33010e
mihaelaVeronica <mihaela.veronica25@yahoo.com>
2016-05-16 21:13:54 -0700

    xml raport

--- tw/Admin/xmlExport.php
+++ tw/Admin/xmlExport.php
@@ -1,10 +1,9 @@
 <?php
-$conn=oci_connect("mihaela","veronica","localhost/XE");
-$xml          = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-$root_element = detalii."s"; 
-$xml         .= "<$root_element>";
+$conn=oci_connect('mihaela','veronica','localhost/XE');
 
-$sql='SELECT * FROM DETALII';
+$xml = new SimpleXMLElement('<flowers/>');
+
+		$sql='SELECT * FROM DETALII';
 			$stid=oci_parse($conn, $sql);
 			oci_execute($stid);
 				while($result_array=oci_fetch($stid)){
@@ -21,21 +20,40 @@ $sql='SELECT * FROM DETALII';
 					$culoare=oci_result($stid, 'CULOARE');
 					$temperatura=oci_result($stid, 'TEMPERATURA');
 					$anotimp=oci_result($stid, 'ANOTIMP');
-					$descriereEngleza=oci_result($stid, 'DESCRIEREENGLEZA');						
-				
-      $xml .= "<".detalii.">";
- 
-      foreach($result_array as $key => $value)
-      {
-         $xml .= "<$key>";
-         $xml .= "<![CDATA[$value]]>";
-         $xml .= "</$key>";
-      }
- 
-      $xml.="</".detalii.">";
+					$descriereEngleza=oci_result($stid, 'DESCRIEREENGLEZA');
+					$descriereFranceza=oci_result($stid, 'DESCRIEREFRANCEZA');					
+					
+					$sql2='SELECT * FROM PLANTE WHERE ID_DETALII=:id_detalii';
+					$stid2=oci_parse($conn, $sql2);
+					oci_bind_by_name($stid2, ':id_detalii', $id_detalii);
+					oci_execute($stid2);
+					while(oci_fetch($stid2)){
+						$denumirePopulara=oci_result($stid2, 'DENUMIREPOPULARA');
+						$denumireStiintifica=oci_result($stid2, 'DENUMIRESTIINTIFICA');
+						$descriere=oci_result($stid2, 'DESCRIERE');
+
+					$flower = $xml->addChild('flower');
+					$flower->addChild('id_detalii', htmlentities("$id_detalii"));
+					$flower->addChild('denumirePopulara',htmlentities("$denumirePopulara"));
+					$flower->addChild('denumireStiintifica', htmlentities("$denumireStiintifica"));
+					$flower->addChild('origine', htmlentities("$origine"));
+					$flower->addChild('localizare', htmlentities("$localizare"));
+					$flower->addChild('caracteristici', htmlentities("$caracteristici"));
+					$flower->addChild('inmultire', htmlentities("$inmultire"));
+					$flower->addChild('imagini', htmlentities("$imagini"));
+					$flower->addChild('beneficii', htmlentities("$beneficii"));
+					$flower->addChild('regimDezvoltare', htmlentities("$regimDezvoltare"));
+					$flower->addChild('perioadaCultivare', htmlentities("$perioadaCultivare"));
+					$flower->addChild('soiuriInrudite', htmlentities("$soiuriInrudite"));
+					$flower->addChild('culoare', htmlentities("$culoare"));
+					$flower->addChild('temperatura', htmlentities("$temperatura"));
+					$flower->addChild('anotimp', htmlentities("$anotimp"));
+					$flower->addChild('descriere', htmlentities("$descriere"));
+					$flower->addChild('descriereEngleza', htmlentities("$descriereEngleza"));
+					$flower->addChild('descriereFranceza', htmlentities("$descriereFranceza"));		
+					}
    }
-}
-$xml .= "</$root_element>";
-header ("Content-Type:text/xml");
-echo $xml;
+
+header ("Content-Type:text");
+print ($xml->asXML());
 ?>
--- tw/Admin/xmlImport.php
+++ tw/Admin/xmlImport.php
@@ -1,36 +1,37 @@
 <?php
-$conn=oci_connect("mihaela","veronica","localhost/XE");
-$xmlDoc = new DOMDocument();
-$xmlDoc->load("bd.xml");
+$conn=oci_connect("mihaela","veronica","localhost");
 
+$xml=simplexml_load_file("db.xml");
 
-$xmlObject = $xmlDoc->getElementsByTagName('ROW');
-$itemCount = $xmlObject->length;
-
-for ($i=0; $i < $itemCount; $i++){
-  $id_detalii = $xmlObject->ROW($i)->getElementsByTagName('id_detalii')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $origine = $xmlObject->ROW($i)->getElementsByTagName('origine')->ROW(0)->childNodes->ROW(0)->nodeValue;
-   $localizare = $xmlObject->ROW($i)->getElementsByTagName('localizare')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $caracteristici  = $xmlObject->ROW($i)->getElementsByTagName('caracteristici')->ROW(0)->childNodes->ROW(0)->nodeValue;
-   $inmultire = $xmlObject->ROW($i)->getElementsByTagName('inmultire')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $imagini = $xmlObject->ROW($i)->getElementsByTagName('imagini')->ROW(0)->childNodes->ROW(0)->nodeValue;
-   $beneficii = $xmlObject->ROW($i)->getElementsByTagName('beneficii')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $regimDezvoltare = $xmlObject->ROW($i)->getElementsByTagName('regimDezvoltare')->ROW(0)->childNodes->ROW(0)->nodeValue;
-   $perioadaCultivare= $xmlObject->ROW($i)->getElementsByTagName('perioadaCultivare')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $soiuriInrudite  = $xmlObject->ROW($i)->getElementsByTagName('soiuriInrudite')->ROW(0)->childNodes->ROW(0)->nodeValue;
-   $culoare = $xmlObject->ROW($i)->getElementsByTagName('culoare')->ROW(0)->childNodes->ROW(0)->nodeValue;
-   $temperatura= $xmlObject->ROW($i)->getElementsByTagName('temperatura')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $anotimp = $xmlObject->ROW($i)->getElementsByTagName('anotimp')->ROW(0)->childNodes->ROW(0)->nodeValue;
-   $descriereEngleza= $xmlObject->ROW($i)->getElementsByTagName('descriereEngleza')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $descriereFranceza = $xmlObject->ROW($i)->getElementsByTagName('descriereFranceza')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $id_planta= $xmlObject->ROW($i)->getElementsByTagName('id_planta')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $denumirePopulara = $xmlObject->ROW($i)->getElementsByTagName('denumirePopulara')->ROW(0)->childNodes->ROW(0)->nodeValue;
-   $denumireStiintifica= $xmlObject->ROW($i)->getElementsByTagName('denumirestiintifica')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $descriere = $xmlObject->ROW($i)->getElementsByTagName('descriereFranceza')->ROW(0)->childNodes->ROW(0)->nodeValue;
-  $sql   = "INSERT INTO detalii (id_detalii, origine, localizare, caracteristici, inmultire, imagini, beneficii, regimDezvoltare, perioadaCultivare, soiuriInrudite, culoare, temperatura, anotimp, descriereEngleza, descriereFranceza) VALUES ('$id_detalii', '$origin', '$localizare', '$caracteristici', '$inmultire', '$imagini', '$beneficii', '$regimDezvoltare', '$perioadaCultivare', '$soiuriInrudite', '$culoare', '$temperatura', '$anotimp', '$descriereEngleza', '$descriereFranceza')";
-     $stid = oci_parse($conn,$sql);
+foreach($xml->children() as $flowers){
+  $id_detalii = $flowers->id_detalii;
+  $origine = $flowers->origine;
+  $localizare = $flowers->localizare;
+  $caracteristici  = $flowers->caracteristici;
+  $inmultire = $flowers->inmultire;
+  $imagini = $flowers->imagini;
+  $beneficii = $flowers->beneficii;
+  $regimDezvoltare = $flowers->regimDezvoltare;
+  $perioadaCultivare = $flowers->perioadaCultivare; 
+  $soiuriInrudite  = $flowers->soiuriInrudite;
+  $culoare = $flowers->culoare;
+  $temperatura = $flowers->temperatura;
+  $anotimp = $flowers->anotimp;
+  $descriereEngleza = $flowers->descriereEngleza;
+  $descriereFranceza = $flowers->descriereFranceza;
+  $id_planta = $flowers->id_planta;
+  $denumirePopulara = $flowers->denumirePopulara;
+  $denumireStiintifica = $flowers->denumireStiintifica;
+  $descriere = $flowers->descriere;
+  
+	$sql   = "INSERT INTO detalii (id_detalii, origine, localizare, caracteristici, inmultire, imagini, beneficii, regimDezvoltare, perioadaCultivare, 
+		     soiuriInrudite, culoare, temperatura, anotimp, descriereEngleza, descriereFranceza) VALUES ('$id_detalii', '$origine', '$localizare', 
+		    '$caracteristici', '$inmultire', '$imagini', '$beneficii', '$regimDezvoltare', '$perioadaCultivare', '$soiuriInrudite', '$culoare', 
+		    '$temperatura', '$anotimp', '$descriereEngleza', '$descriereFranceza')";
+    $stid = oci_parse($conn,$sql);
     $r = oci_execute($stid);
-    $sql1 = "INSERT INTO PLANTE(id_planta,id_detalii,denumirePopulara,denumireStiintifica,imagine,descriere) VALUES ('$id_planta','$id_detalii','$denumirePopulara','$denumireStiintifica','$imagine','$descriere')";
+    $sql1 = "INSERT INTO PLANTE(id_planta,id_detalii,denumirePopulara,denumireStiintifica,imagine,descriere) VALUES ('$id_planta','$id_detalii',
+	         '$denumirePopulara','$denumireStiintifica','$imagine','$descriere')";
     $stid1 = oci_parse($conn,$sql1);
     $r1 = oci_execute($stid1);
 			
@@ -46,7 +47,6 @@ for ($i=0; $i < $itemCount; $i++){
                 echo 'Data was not inserted...<br>';
             }
 	}
-}
-}
+
 echo "<a href='admin.php'>Back to admin page</a>";
 ?>
