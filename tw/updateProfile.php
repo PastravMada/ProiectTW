d44330ce4f26ef65d0832791672008323834b11f
madalinavieru11 <mada_lina383@yahoo.com>
2016-05-21 21:53:14 +0300

    Update updateProfile.php

--- tw/updateProfile.php
+++ tw/updateProfile.php
@@ -8,58 +8,70 @@ session_start();
 		echo "You have not signed in";
 	}
 ?>
-<html>
-<head>
-<title>Update Profile</title>
-    <link rel="shortcut icon" type="image/x-icon" href="logo.ico" />
-	<link rel="stylesheet" type="text/css" href="Incluziuni/incluziuni.css">
-	<link rel="stylesheet" type="text/css" href="css/login.css">
-	<link rel="stylesheet" href="Admin/cssMenu/layout.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="Admin/cssMenu/menu.css" type="text/css" media="screen">
-</head>
-<body>
-    <?php include('Incluziuni/headerProfile.php');?>
-<h2 align=center color=red>Update your profile</h2>
-<div align=center>
-<form action="updated.php?" name="frmEdit" method="post">
 
-<?php
-$conn=oci_connect("mihaela","veronica","localhost"); 
 	
-$sql = "SELECT * FROM USERI WHERE username='$username'";
-$stid = oci_parse($conn,$sql);
-oci_execute($stid);
-?>
 <?php
-while($row = oci_fetch_array($stid, OCI_BOTH)) {
-	$id_user=$row[1];
-?>
 
-<table width="550" border="0">
-    <tr><th width="100" height="50" style="background-color:#BDB76B;color:black;"> <div align="left">Username</div></th>
-    <th><input class="heighttext"type="text" name="username" size="16" value="<?=$row[1];?>"></th></tr>
-    <tr><th width="65" height="50" style="background-color:#BDB76B;color:black;"> <div align="left">Password</div></th>
-    <th><input class="heighttext" type="text" name="password" size="16" value="<?=$row[2];?>"></th></tr>
-    <tr><th width="100" height="50" style="background-color:#BDB76B;color:black;"> <div align="left">Nume</div></th>
-	<th><input class="heighttext" type="text" name="nume" size="16" value="<?=$row[3];?>"></th></tr>
-    <tr><th width="59" height="50" style="background-color:#BDB76B;color:black;"> <div align="left">Prenume</div></th>
-	<th ><input class="heighttext" type="text" name="prenume" size="16" value="<?=$row[4];?>"></th></tr>
-    <tr><th width="7" height="50" style="background-color:#BDB76B;color:black;"> <div align="left">Email</div></th>
-	<th><input class="heighttext" type="text" name="email" size="16" value="<?=$row[5];?>"></th></tr>
-    <tr><th width="60" height="50" style="background-color:#BDB76B;color:black;"> <div align="left">Sex</div></th>
-	<th><input class="heighttext" type="text" name="sex" size="16" value="<?=$row[6];?>"></th></tr>
-	<tr><th width="60" height="50" style="background-color:#BDB76B;color:black;"> <div align="left">Adresa</div></th>
-	<th><input class="heighttext" type="text" name="adresa" size="16" value="<?=$row[7];?>"></th></tr>
-	<tr><th width="100" height="50" style="background-color:#BDB76B;color:black;"> <div align="left">Telefon</div></th>
-	<th><input class="heighttext" type="text" name="telefon" size="16" value="<?=$row[8];?>"></th></tr>
-    <th><input type="hidden" name="id_user" size="16" value="<?=$row[0];?>"></th></tr>
-</table>
-<br/>
-<input class="btn" type="submit" name="submit" value="UPDATE">
+$c1 = oci_connect("mihaela", "veronica", 'localhost/XE');
 
-<?php
-}
 ?>
+<link rel="shortcut icon" type="image/x-icon" href="logo.ico" />
+	<link rel="stylesheet" type="text/css" href="IncluziuniAdmin/incluziuniAdmin.css">
+	<link rel="stylesheet" href="cssMenu/layout.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="cssMenu/menu.css" type="text/css" media="screen">
+	<form name="register_form" method="post">
+    <table width="50%" border="0" cellpadding="0" cellspacing="2">
+<?php include('IncluziuniAdmin/headerAdmin.php');?>
+	 <form action="action_page.php">
+ <tr>
+       <td width="100%">Username-ul introdus de dumneavoastra va fi admin:</td>
+    </tr>
+            <td width="40%">Username</td>
+            <td><input type="text" name="Username" id="Username" /></td>
+   </tr>
+   <tr>
+           Sunteti sigur?
+    <input type="checkbox" name="formWheelchair" value="Yes" />
+</tr><br><tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="btn_i" id="in" value="Adauga" /></td>
+        </tr>
+    </table>
 </form>
-</body>
-</html>
+</form>
+<?php 
+function update($connname, $conn)
+{ 	
+		$strSQL = "UPDATE Useri SET Username='Admin' WHERE username= :username ";
+    $stmt = oci_parse($conn, $strSQL);
+	oci_bind_by_name($stmt, ":username", $username);
+    oci_execute($stmt, OCI_DEFAULT);
+
+}
+
+
+if(isset($_POST['formWheelchair']) && 
+   $_POST['formWheelchair'] == 'Yes')
+{
+	update('c1',$c1);
+
+
+$s1 = ociparse($c1, "SELECT * FROM useri");
+    echo '<br />';
+    if(ociexecute($s1))
+    {
+        while (ocifetch($s1)) 
+        {
+			
+                        
+        }
+    }
+    else
+    {
+	$e = oci_error($s1); 
+        echo htmlentities($e['message']);
+    }
+	sleep(2);
+
+	
+}
