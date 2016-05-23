1b64586acb46d685346e54ce244e48aa5533bc0b
madalinavieru11 <mada_lina383@yahoo.com>
2016-05-23 21:18:28 +0300

    Update addAdmin.php

--- tw/Admin/addAdmin.php
+++ tw/Admin/addAdmin.php
@@ -1,33 +1,37 @@
 
-<?php
 
+<?php
 $c1 = oci_connect("mihaela", "veronica", 'localhost/XE');
 $username = isset($_POST['username']) ? $_POST['username'] : '';
 ?>
+<html>
+<head>
 <link rel="shortcut icon" type="image/x-icon" href="logo.ico" />
 	<link rel="stylesheet" type="text/css" href="IncluziuniAdmin/incluziuniAdmin.css">
 	<link rel="stylesheet" href="cssMenu/layout.css" type="text/css" media="screen">
     <link rel="stylesheet" href="cssMenu/menu.css" type="text/css" media="screen">
-	<form name="register_form" method="post">
-    <table width="50%" border="0" cellpadding="0" cellspacing="2">
-<?php include('IncluziuniAdmin/headerAdmin.php');?>
-<head>
-  <title>Add admin</title>
-  <link href="admin1.css" rel="stylesheet">
+	
+    <title>Add admin</title>
+    <link href="admin1.css" rel="stylesheet">
 </head>
-	 <form >
-	 <div id="main" class="container">
-       <h1>Username-ul introdus de dumneavoastra va fi admin:</h1>
-	   <div class="container">
+<body>
+<?php include('IncluziuniAdmin/headerAdmin.php');?>
+<table width="50%" border="0" cellpadding="0" cellspacing="2">
+<form name="register_form" method="post">   
+	 <div id="main" class="container1">
+       <h2>Username-ul introdus de dumneavoastra va fi admin:</h2>
+	   <div class="container1" >
            <tr> <tr><h2>Username
             <input type="text" name="username" id="username" /></h2></tr>
+			<br/>
           <tr><h3 >Sunteti sigur?
+		  
     <input type="checkbox" name="formWheelchair" value="Yes" /></h3></tr>
+	<br/>
             <tr><input  type="submit" name="btn_i" id="button" value="Adauga" style="display: block; margin: 0 auto;"/></tr><tr>
 			</div>
        </div>
    </table>
-</form>
 </form>
 <?php 
 function update($connname, $conn)
@@ -38,16 +42,11 @@ function update($connname, $conn)
     $r=oci_execute($stmt, OCI_DEFAULT);
                
 }
-
 }
-
-
 if(isset($_POST['formWheelchair']) && 
    $_POST['formWheelchair'] == 'Yes')
 {
 	update('c1',$c1);
-
-
 $s1 = ociparse($c1, "SELECT * FROM useri WHERE username='$username'");
     echo '<br />';
     if(ociexecute($s1))
@@ -64,7 +63,7 @@ $s1 = ociparse($c1, "SELECT * FROM useri WHERE username='$username'");
         echo htmlentities($e['message']);
     }
 	sleep(2);
-
-	
 }
-
+?>
+</body>
+</html>
